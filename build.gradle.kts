2a4116cd48fa496ed6ca3ffdbe51a2297e2402ad
SecretX33 <4389757+SecretX33@users.noreply.github.com>
2023-10-20 09:59:00 -0300

    Fix invalid main class in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,9 +18,7 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation(kotlin("reflect"))
-    implementation("io.github.secretx33:path-matching-resource-pattern-resolver:0.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-
     implementation("ch.qos.logback:logback-classic:1.4.11")
     implementation(platform("com.fasterxml.jackson:jackson-bom:2.15.2"))
     implementation("com.fasterxml.jackson.core:jackson-databind")
@@ -28,8 +26,9 @@ dependencies {
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jdk8")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("com.squareup.okhttp3:okhttp:4.11.0")
     implementation("com.github.davidmoten:subethasmtp:6.0.7")
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+    implementation("io.github.secretx33:path-matching-resource-pattern-resolver:0.1")
 }
 
 tasks.test { useJUnitPlatform() }
@@ -56,18 +55,8 @@ tasks.withType<KotlinCompile> {
     }
 }
 
-val mainClassName = "com.github.secretx33.mailexample.MainKt"
+val mainClassName = "com.github.secretx33.mailexample.ServerKt"
 
 application {
     mainClass.set(mainClassName)
-}
-
-// Enables the usage of Java classes inside 'kotlin' package
-sourceSets {
-    val allSourceMain = listOf("src/main/java", "src/main/kotlin")
-    main {
-        listOf(java, kotlin).forEach {
-            it.setSrcDirs(allSourceMain)
-        }
-    }
 }
